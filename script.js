31ad0445107b086ca4106001a7e800a25df027f3
Anil Seervi <anil13112000@gmail.com>
2020-09-15 23:09:59 +0530

    initial project

---
+++ script.js
@@ -0,0 +1,15 @@
+const inputText = document.querySelector("#textInput");
+const output = document.querySelector(".output");
+
+function countWords(string) {
+  return string.trim().split(/\s+/).length;
+}
+
+inputText.addEventListener("keyup", () => {
+  let str = inputText.value;
+  if (str === "") {
+    output.textContent = "0";
+  } else {
+    output.textContent = countWords(str);
+  }
+});
